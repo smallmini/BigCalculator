1ee87a43c2004a0584946b70ce71cd4fd382e26e
LiXiaYu <aasll@126.com>
2015-10-18 22:49:59 +0800

    v1.0.0
    只能算简单的加法。。。

---
+++ BigCalculator/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BigCalculator
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            BigNum num1 = new BigNum(this.txtNum1.Text);
+            BigNum num2 = new BigNum(this.txtNum2.Text);
+
+            BigNum sum = num1.Add(num2);
+
+            this.lblResult.Content = sum.Value();
+        }
+    }
+}
